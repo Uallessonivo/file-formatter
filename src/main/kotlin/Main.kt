7672f9372159991e96d505d7899253222f281dae
Uallessonivo <uallessons@gmail.com>
2022-12-23 14:26:09 -0300

    done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 data class DataParse(
     val nSerie: String,
@@ -10,26 +12,60 @@ data class DataParse(
 ) {
     companion object {
         fun parse(element: Row) = DataParse(
-            nSerie = element.getCell(1).toString(),
-            nCpf = element.getCell(2).toString().replace("[.-]".toRegex(), ""),
+            nSerie = element
+                .getCell(1)
+                .toString(),
+            nCpf = element
+                .getCell(2)
+                .toString()
+                .replace("[.-]".toRegex(), ""),
             nValue = " ",
-            nName = element.getCell(3).toString().uppercase()
+            nName = element
+                .getCell(3)
+                .toString()
+                .let {
+                    val (fName, sName) = it.split("\\s".toRegex()).toTypedArray()
+                    "$fName $sName"
+                }
+                .trim()
+                .uppercase()
         )
     }
 }
 
-fun readExcelFile(filePath: String) {
-    val inputStream = FileInputStream(filePath)
-    val xlWb = XSSFWorkbook(inputStream)
-        .getSheetAt(0)
-        .map {
-            DataParse.parse(it.first().row)
-        }
+fun parseExcelFile(filePath: String): List<DataParse> {
+    try {
+        val inputStream = FileInputStream(filePath)
+        val xlWb = XSSFWorkbook(inputStream)
+            .getSheetAt(0)
+            .drop(1)
+            .map {
+                DataParse.parse(it.first().row)
+            }
+        return xlWb
+    } catch (e: UnsupportedOperationException) {
+        throw UnsupportedOperationException("Falha durante parse do arquivo de origem.")
+    }
+}
 
-    println(xlWb)
+fun OutputStream.writeCsv(parsedRow: List<DataParse>) {
+    try {
+        val wr = bufferedWriter()
+        wr.write("""Numero de Serie, CPF, Valor da Carga, Observacao""")
+        wr.newLine()
+        parsedRow.forEach {
+            wr.write("${it.nSerie}, ${it.nCpf}, ${it.nValue}, ${it.nName}")
+            wr.newLine()
+        }
+        wr.flush()
+    } catch (e: UnsupportedOperationException) {
+        throw UnsupportedOperationException("Falha durante a gravação do arquivo.")
+    }
 }
 
 fun main(args: Array<String>) {
     val filePath = "files/cartoes.xlsx"
-    readExcelFile(filePath)
+    val parsedRows = parseExcelFile(filePath)
+    FileOutputStream("files/output.csv").apply { writeCsv(parsedRows) }
+    println("Arquivo gerado com sucesso.")
 }
